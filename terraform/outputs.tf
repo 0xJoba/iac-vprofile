4120e1a86f3fa6b5f2d2f033ea02d27e3f91a97a
0xJoba <Distinctwhale@gmail.com>
2026-05-05 19:26:41 +0100

    Fix: Remove duplicate output and fix brace

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -16,3 +16,4 @@ output "region" {
 output "cluster_security_group_id" {
   description = "Security group attached to the cluster control plane"
   value       = module.eks.cluster_security_group_id
+}
